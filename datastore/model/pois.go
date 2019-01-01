4a08483fbfc2e514c7e665c7c563ef4643d4a194
Fotis Papadopoulos <fpapadopou@gmail.com>
2019-01-01 15:22:27 +0200

    Add POI database model

---
+++ datastore/model/pois.go
@@ -0,0 +1,20 @@
+package model
+
+import (
+	"time"
+)
+
+// POIs struct represents a point of interest object
+// This is a shareable model that can be used by all database providers (Postgres, MongoDB, etc)
+// The struct also contains metadata that dictates how the object properties should be
+// handled during JSON encoding/decoding
+type POIs struct {
+	ID        PrimaryKey `json:"id,omitempty"`
+	Name      string     `json:"name,omitempty"`
+	Longitude float64    `json:"lon,omitempty"`
+	Latitude  float64    `json:"lat,omitempty"`
+	Location  string     `json:"location,omitempty"`
+	Category  string     `json:"category,omitempty"`
+	CreatedAt time.Time  `json:"created_at,omitempty"`
+	UpdatedAt time.Time  `json:"updated_at,omitempty"`
+}
