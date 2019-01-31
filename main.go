f358bbd5a7cc84b054d5bf67bc3e7931c821542d
Fotis Papadopoulos <fpapadopou@gmail.com>
2019-02-01 01:16:20 +0200

    Introduce Server dependency container struct & use datastore in POI GET methods

--- main.go
+++ main.go
@@ -6,18 +6,18 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
-	config "github.com/fpapadopou/poi/config"
+	"github.com/fpapadopou/poi/config"
+	"github.com/fpapadopou/poi/datastore"
 	"github.com/gorilla/mux"
 )
 
-// POI represents a point-of-interest
-type POI struct {
-	ID   string `json:"id,omitempty"`
-	Name string `json:"name,omitempty"`
+// Server encapsulates router & datastore related structs
+type Server struct {
+	router *mux.Router
+	ds     *datastore.DS
 }
-
-var pois []POI
 
 // Main application function
 func main() {
@@ -25,64 +25,71 @@ func main() {
 	appConfig := config.GetApplicationConfig()
 
 	// Create a router and register all routes
-	router := mux.NewRouter()
-	registerRoutes(router)
+	server := &Server{
+		router: mux.NewRouter(),
+		ds:     datastore.NewDS(config.GetDatabaseConfig()),
+	}
+	server.registerRoutes()
 
 	// Serve POIs to the people
-	if err := http.ListenAndServe(":"+appConfig.Port, router); err != nil {
+	if err := http.ListenAndServe(":"+appConfig.Port, server.router); err != nil {
 		log.Fatalf("Failed to start application: %v", err)
 	}
 	fmt.Println("Waiting for connections..")
 }
 
 // Register available routes to router
-func registerRoutes(router *mux.Router) {
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+func (s *Server) registerRoutes() {
+	s.router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write(bytes.NewBufferString("Hiya!").Bytes())
 	}).Methods(http.MethodGet)
 
-	router.HandleFunc("/pois", GetPOIs).Methods(http.MethodGet)
-	router.HandleFunc("/pois/{id:[0-9]+}", GetPOIByID).Methods(http.MethodGet)
-	router.HandleFunc("/pois", CreatePOI).Methods(http.MethodPost)
-	router.HandleFunc("/pois/{id:[0-9]+}", UpdatePOI).Methods(http.MethodPut)
-	router.HandleFunc("/pois/{id:[0-9]+}", DeletePOI).Methods(http.MethodDelete)
+	s.router.HandleFunc("/poi", s.GetPOIs()).Methods(http.MethodGet)
+	s.router.HandleFunc("/poi/{id:[0-9]+}", s.GetPOIByID).Methods(http.MethodGet)
+	s.router.HandleFunc("/poi", s.CreatePOI).Methods(http.MethodPost)
+	s.router.HandleFunc("/poi/{id:[0-9]+}", s.UpdatePOI).Methods(http.MethodPut)
+	s.router.HandleFunc("/poi/{id:[0-9]+}", s.DeletePOI).Methods(http.MethodDelete)
 }
 
 // CreatePOI creates a new point-of-interest with the provided data
-func CreatePOI(w http.ResponseWriter, r *http.Request) {
+func (s *Server) CreatePOI(w http.ResponseWriter, r *http.Request) {
 	// TODO: Add validator for create/update
 	fmt.Println("POI created")
 	w.WriteHeader(http.StatusCreated)
 }
 
 // GetPOIs returns all available POIs from database
-func GetPOIs(w http.ResponseWriter, r *http.Request) {
-	pois = nil
-	pois = append(pois, POI{ID: "12", Name: "First POI"})
-	pois = append(pois, POI{ID: "24", Name: "Second POI"})
+func (s *Server) GetPOIs() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		pois, _ := s.ds.DatabaseProvider.GetAll()
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(pois)
+	}
+}
 
-	w.Header().Add("Content-Type", "application/json")
+// GetPOIByID returns the specified POI
+func (s *Server) GetPOIByID(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	ID, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		log.Fatal("Invalid POI ID")
+	}
+	poi, _ := s.ds.DatabaseProvider.GetPOIByID(ID)
+
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(pois)
+	json.NewEncoder(w).Encode(poi)
 }
 
 // UpdatePOI creates a new point-of-interest with the provided data
-func UpdatePOI(w http.ResponseWriter, r *http.Request) {
+func (s *Server) UpdatePOI(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("POI updated")
 	w.WriteHeader(http.StatusOK)
 }
 
-// GetPOIByID returns the specified POI
-func GetPOIByID(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(POI{ID: vars["id"], Name: "POI by its ID"})
-}
-
 // DeletePOI deletes an existing POI from the database
-func DeletePOI(w http.ResponseWriter, r *http.Request) {
+func (s *Server) DeletePOI(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("POI deleted")
 	w.WriteHeader(http.StatusOK)
 }
